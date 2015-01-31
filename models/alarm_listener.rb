9926043da671b993186d7f9f8254b8a28f12400a
Felix Abele <felix.abele@googlemail.com>
2015-01-31 21:35:30 +0100

    store and use output and inout pins

--- models/alarm_listener.rb
+++ models/alarm_listener.rb
@@ -8,15 +8,22 @@ class AlarmListener
 
   def start!
     proc_id = Process.fork do
-      button = 1
+      listen_pin = @settings.data[:input_pin].to_i
+      activate_pin = @settings.data[:output_pin].to_i
       gpio = WiringPi::GPIO.new
-      gpio.mode button, INPUT
+      gpio.mode( listen_pin, INPUT) if listen_pin > 0
+
+      if activate_pin > 0
+        gpio.mode( activate_pin, OUTPUT)
+        gpio.write activate_pin, 0
+      end
 
       loop do
         state = gpio.readAll
 
-        if state[button] == 1
+        if state[listen_pin] == 1
           RestClient.post 'http://localhost:4567/api/start'
+          gpio.write( activate_pin, 1 ) if activate_pin > 0
         end
         sleep 0.3
       end
